eb1fd5aa3c16919d1494bd02002fd4de9813a4fb
youmu178 <youmu178@sina.com>
2014-05-26 16:29:31 +0800

    Merge branch 'master' of https://github.com/ITBox/GaoRenZhiLu
    Conflicts:
    	code/ITBoxFramework/src/com/itbox/fx/util/Utils.java

--- code/ITBoxFramework/src/com/itbox/fx/util/Utils.java
+++ code/ITBoxFramework/src/com/itbox/fx/util/Utils.java
@@ -9,20 +9,6 @@ import android.widget.EditText;
 public class Utils {
 
 	/**
-	 * 弹出软键盘
-	 * 
-	 * @param et
-	 */
-	public static void showKeyboard(final EditText et) {
-		et.setFocusable(true);
-		et.setFocusableInTouchMode(true);
-		et.requestFocus();
-		InputMethodManager inputManager = (InputMethodManager) et.getContext()
-				.getSystemService(Context.INPUT_METHOD_SERVICE);
-		inputManager.showSoftInput(et, 0);
-	}
-
-	/**
 	 * 隐藏软键盘
 	 * 
 	 * @param activity
